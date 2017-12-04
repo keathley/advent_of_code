a63d637836a5b9e8828037514e36bcef39764ce9
Chris Keathley <c@keathley.io>
2017-12-04 09:18:42 -0500

    Complete solution for day 4

--- lib/advent_of_code/day_four.ex
+++ lib/advent_of_code/day_four.ex
@@ -1,16 +1,39 @@
 defmodule AdventOfCode.DayFour do
   def part_one(string) do
     string
+    |> parse
+    |> valid_count(& &1)
+  end
+
+  def part_two(string) do
+    string
+    |> parse
+    |> valid_count(&lexically_order/1)
+  end
+
+  def parse(string) do
+    string
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split/1)
-    |> Enum.map(&valid?/1)
+  end
+
+  def valid_count(lines, f) do
+    lines
+    |> Enum.map(& valid?(&1, f))
     |> Enum.filter(& &1)
     |> Enum.count
   end
 
-  def valid?(words) do
+  def lexically_order(word) do
+    word
+    |> String.graphemes
+    |> Enum.sort
+    |> Enum.join
+  end
+
+  def valid?(words, f) do
     words
-    |> Enum.uniq
+    |> Enum.uniq_by(f)
     |> Enum.count == Enum.count(words)
   end
 end
